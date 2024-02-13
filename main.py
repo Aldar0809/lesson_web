22a7fad2a40dfe2556c6504d131fb50f725ec8f9
Aldar <129419263+Aldar0809@users.noreply.github.com>
2024-02-13 13:06:47 +0300

    list_prof
    list_prof

--- main.py
+++ main.py
@@ -2,17 +2,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route('/list_prof/<string:list_type>')
+def list_prof(list_type):
+    professions = ["Engineer", "Doctor", "Astronaut", "Scientist", "Geologist"]
+    if list_type == 'ol':
+        return render_template('list.html', professions=professions, list_type='ol')
+    elif list_type == 'ul':
+        return render_template('list.html', professions=professions, list_type='ul')
+    else:
+        return "Invalid parameter. Please use 'ol' for ordered list or 'ul' for unordered list."
 
-@app.route('/<title>')
-@app.route('/index/<title>')
-def index(title):
-    return render_template('base.html', title=title)
-
-
-@app.route('/training/<prof>')
-def profes(prof):
-    return render_template('train.html', job=prof)
-
-
-if __name__ == '__main__':
-    app.run(port=8000, host='127.0.0.1')
+if __name__ == "__main__":
+    app.run(port=8000, host='147.45.77.43')
